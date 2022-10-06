f1d83e7e7581c6a8aaaf7ba87b8fe3bd3ad31d93
Thomas Girod <thgirod@hotmail.com>
2022-10-07 00:17:12 +0200

    black + fix regex

--- eboutic/forms.py
+++ eboutic/forms.py
@@ -61,7 +61,8 @@ class BasketForm:
     without calling `form.clean()`. In this case, the latter method shall be
     implicitly called.
     """
-    flat_json_re = re.compile(r"^\[\{[^{}\]\[]*\},?\s*\]$")
+
+    flat_json_re = re.compile(r"^\[\s*(\{[^{}\]\[]*\},?\s*)*\s*\]$")
 
     def __init__(self, request: HttpRequest):
         self.user = request.user
